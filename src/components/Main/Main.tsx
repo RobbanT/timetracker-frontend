60e735ed12647b5da920a6c73f7f1d915f14f607
Roberto Tencic <roberto.tencic@outlook.com>
2024-04-28 18:04:17 +0200

    Uppdatering.

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -10,14 +10,46 @@ function Main(props: Props) {
             <div className="container">
                 <h2>{props.page}</h2>
                 {{
-                    home: null,
-                    reservation: null,
-                    staff: null,
-                    about: null,
-                }[props.page] || null}
+                    Hem: <RandomArticle />,
+                    Tidsuppföljning: <TimeTracking />,
+                    Funktioner: <RandomArticle />,
+                    Om: <RandomArticle />,
+                }[props.page] || <RandomArticle />}
             </div>
         </main>
     );
+}
+
+function RandomArticle() {
+    return (
+        <article>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam quos officia ab vitae. Exercitationem natus labore, dicta aliquid ab eaque alias perspiciatis excepturi! Suscipit
+                necessitatibus sint, sed eaque ipsam in? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae neque quo maiores illo, labore minima tempore veritatis quaerat
+                asperiores fuga qui recusandae distinctio tempora corrupti dolorum nobis numquam, impedit ex? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam quos officia ab vitae.
+                Exercitationem natus labore, dicta aliquid ab eaque alias perspiciatis excepturi! Suscipit necessitatibus sint, sed eaque ipsam in? Lorem ipsum dolor, sit amet consectetur adipisicing
+                elit. Repudiandae neque quo maiores illo, labore minima tempore veritatis quaerat asperiores fuga qui recusandae distinctio tempora corrupti dolorum nobis numquam, impedit ex?
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam quos officia ab vitae. Exercitationem natus labore, dicta aliquid ab eaque alias perspiciatis excepturi! Suscipit
+                necessitatibus sint, sed eaque ipsam in? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae neque quo maiores illo, labore minima tempore veritatis quaerat
+                asperiores fuga qui recusandae distinctio tempora corrupti dolorum nobis numquam, impedit ex? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam quos officia ab vitae.
+                Exercitationem natus labore, dicta aliquid ab eaque alias perspiciatis excepturi! Suscipit necessitatibus sint, sed eaque ipsam in? Lorem ipsum dolor, sit amet consectetur adipisicing
+                elit. Repudiandae neque quo maiores illo, labore minima tempore veritatis quaerat asperiores fuga qui recusandae distinctio tempora corrupti dolorum nobis numquam, impedit ex?
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam quos officia ab vitae. Exercitationem natus labore, dicta aliquid ab eaque alias perspiciatis excepturi! Suscipit
+                necessitatibus sint, sed eaque ipsam in? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae neque quo maiores illo, labore minima tempore veritatis quaerat
+                asperiores fuga qui recusandae distinctio tempora corrupti dolorum nobis numquam, impedit ex? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam quos officia ab vitae.
+                Exercitationem natus labore, dicta aliquid ab eaque alias perspiciatis excepturi! Suscipit necessitatibus sint, sed eaque ipsam in? Lorem ipsum dolor, sit amet consectetur adipisicing
+                elit. Repudiandae neque quo maiores illo, labore minima tempore veritatis quaerat asperiores fuga qui recusandae distinctio tempora corrupti dolorum nobis numquam, impedit ex?
+            </p>
+        </article>
+    );
+}
+
+function TimeTracking() {
+    return <></>;
 }
 
 export default Main;
