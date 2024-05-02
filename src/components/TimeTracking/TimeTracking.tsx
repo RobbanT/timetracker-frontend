5728c0011548f07e308adfdae15a7c4c137ee7ca
Roberto Tencic <roberto.tencic@outlook.com>
2024-05-02 16:37:31 +0200

    Fix.

--- src/components/TimeTracking/TimeTracking.tsx
+++ src/components/TimeTracking/TimeTracking.tsx
@@ -1,7 +1,17 @@
+import Calendar from "react-calendar";
+import { useState } from "react";
 import "./style.css";
 
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 function TimeTracking() {
-    return <div className="container"></div>;
+    const [value, onChange] = useState<Value>(new Date());
+    return (
+        <>
+            <Calendar onChange={onChange} showNeighboringMonth={false} prev2Label={null} next2Label={null} minDate={new Date()} maxDate={new Date(2025, 0, 0)} minDetail={"month"} value={value} />
+        </>
+    );
 }
 
 export default TimeTracking;
