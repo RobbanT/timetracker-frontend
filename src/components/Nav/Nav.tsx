f6e573c4574c60c4535f9a46b1af1a4de172e529
Roberto Tencic <roberto.tencic@outlook.com>
2024-05-02 13:17:54 +0200

    Fix.

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -4,12 +4,29 @@ interface Props {
     setPage: (page: string) => void;
 }
 
+interface Task {
+    title: string;
+    date: string;
+    startTime: string;
+    endTime: string;
+}
+
+interface User {
+    username: string;
+    password: string;
+    tasks: Task[];
+}
+
+function loadUser(): User {
+    return JSON.parse(localStorage.getItem("user") as string);
+}
+
 function Nav(props: Props) {
     return (
         <nav>
             <a onClick={() => props.setPage("Hem")}>Hem</a>
             <a onClick={() => props.setPage("Tidsrapportering")}>Tidsrapportering</a>
-            <a onClick={() => props.setPage("Medlemmar")}>Medlemmar</a>
+            {loadUser() != null && loadUser().username == "admin" ? <a onClick={() => props.setPage("Medlemmar")}>Medlemmar</a> : null}
             <a onClick={() => props.setPage("Logga in")}>{localStorage.getItem("cart") != null ? "Logga in" : "Logga ut"}</a>
             <a onClick={() => props.setPage("Bli medlem")}>Bli medlem</a>
         </nav>
