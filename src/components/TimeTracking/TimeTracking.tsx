998361185b4320361db71cec5fda8717ed88db8c
Roberto Tencic <roberto.tencic@outlook.com>
2024-05-02 20:10:02 +0200

    Fix.

--- src/components/TimeTracking/TimeTracking.tsx
+++ src/components/TimeTracking/TimeTracking.tsx
@@ -5,6 +5,23 @@ import "./style.css";
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
+interface Task {
+    title: string;
+    date: string;
+    startTime: string;
+    endTime: string;
+}
+
+interface User {
+    username: string;
+    password: string;
+    tasks: Task[];
+}
+
+function loadUser(): User {
+    return JSON.parse(localStorage.getItem("user") as string);
+}
+
 function TimeTracking() {
     const [value, onChange] = useState<Value>(new Date());
     return (
@@ -12,6 +29,13 @@ function TimeTracking() {
             <Calendar onChange={onChange} showNeighboringMonth={false} prev2Label={null} next2Label={null} minDate={new Date()} maxDate={new Date(2025, 0, 0)} minDetail={"month"} value={value} />
             <div className="inner-container">
                 <h3>Uppgifter</h3>
+                <ul>
+                    {loadUser().tasks.map((task: Task) => (
+                        <li>
+                            <p>{task.title}</p>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </>
     );
