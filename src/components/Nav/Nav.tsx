11c24fb4818a9fbb457bf743405067e3c171a98c
Roberto Tencic <roberto.tencic@outlook.com>
2024-05-02 13:26:08 +0200

    Fix.

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -28,7 +28,7 @@ function Nav(props: Props) {
             <a onClick={() => props.setPage("Tidsrapportering")}>Tidsrapportering</a>
             {loadUser() != null && loadUser().username == "admin" ? <a onClick={() => props.setPage("Medlemmar")}>Medlemmar</a> : null}
             <a onClick={() => props.setPage("Logga in")}>{loadUser() == null ? "Logga in" : "Logga ut"}</a>
-            <a onClick={() => props.setPage("Bli medlem")}>Bli medlem</a>
+            {loadUser() == null ? null : <a onClick={() => props.setPage("Bli medlem")}>Bli medlem</a>}
         </nav>
     );
 }
