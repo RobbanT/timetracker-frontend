422570d25d5cab700c007677dcc694a3bd8fb599
Roberto Tencic <roberto.tencic@outlook.com>
2024-05-02 11:53:41 +0200

    Test.

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,12 +1,47 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import "./style.css";
 
+interface Inputs {
+    username: string;
+    password: string;
+}
+
+interface Task {
+    title: string;
+    date: string;
+    startTime: string;
+    endTime: string;
+}
+
+interface Member {
+    username: string;
+    password: string;
+    tasks: Task[];
+}
+
 function Login() {
+    const [member, setMember] = useState<Member>();
+    const [inputs, setInputs] = useState<Inputs>({
+        username: "",
+        password: "",
+    });
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => setInputs((values) => ({ ...values, [event.target.name]: event.target.value }));
+
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        fetch(`https://backend-eft68.ondigitalocean.app/user/${inputs.username}`)
+            .then((res) => res.json())
+            .then((data) => setMember(data));
+        console.log(member);
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Användarnamn</label>
-            <input placeholder="Ange användarnamn..." maxLength={64} required></input>
+            <input placeholder="Ange användarnamn..." value={inputs.username} name="username" onChange={handleChange} maxLength={64} required></input>
             <label>Lösenord</label>
-            <input placeholder="Ange lösenord..." type="password" minLength={12} maxLength={64} required></input>
+            <input placeholder="Ange lösenord..." value={inputs.password} name="password" onChange={handleChange} type="password" minLength={12} maxLength={64} required></input>
             <button type="submit">Logga in</button>
         </form>
     );
