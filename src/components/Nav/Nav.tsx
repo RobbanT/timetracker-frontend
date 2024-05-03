5378f7338078cc6c2f3f689b231e03edff22dfaa
Roberto Tencic <roberto.tencic@outlook.com>
2024-05-04 00:14:20 +0200

    Fix.

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,23 +1,11 @@
+import { Member } from "../Main";
 import "./style.css";
 
 interface Props {
     setPage: (page: string) => void;
 }
 
-interface Task {
-    title: string;
-    date: string;
-    startTime: string;
-    endTime: string;
-}
-
-interface User {
-    username: string;
-    password: string;
-    tasks: Task[];
-}
-
-function loadUser(): User {
+function loadMember(): Member {
     return JSON.parse(localStorage.getItem("user") as string);
 }
 
@@ -25,10 +13,10 @@ function Nav(props: Props) {
     return (
         <nav>
             <a onClick={() => props.setPage("Hem")}>Hem</a>
-            {loadUser() != null ? <a onClick={() => props.setPage("Tidsrapportering")}>Tidsrapportering</a> : null}
-            {loadUser() != null && loadUser().username == "admin" ? <a onClick={() => props.setPage("Medlemmar")}>Medlemmar</a> : null}
-            <a onClick={() => props.setPage("Logga in")}>{loadUser() == null ? "Logga in" : "Logga ut"}</a>
-            {loadUser() != null ? null : <a onClick={() => props.setPage("Bli medlem")}>Bli medlem</a>}
+            {loadMember() != null ? <a onClick={() => props.setPage("Tidsrapportering")}>Tidsrapportering</a> : null}
+            {loadMember() != null && loadMember().username == "admin" ? <a onClick={() => props.setPage("Medlemmar")}>Medlemmar</a> : null}
+            <a onClick={() => props.setPage("Logga in")}>{loadMember() == null ? "Logga in" : "Logga ut"}</a>
+            {loadMember() != null ? null : <a onClick={() => props.setPage("Bli medlem")}>Bli medlem</a>}
         </nav>
     );
 }
