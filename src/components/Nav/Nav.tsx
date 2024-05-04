d6405105ba0a8cc580b2a5eea4a3224e96b7ffaf
Roberto Tencic <roberto.tencic@outlook.com>
2024-05-04 10:28:20 +0200

    Kan både logga in och registrera nu.

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -19,18 +19,11 @@ function Nav(props: Props) {
             <a onClick={() => props.setPage("Hem")}>Hem</a>
             {loadMember() != null ? <a onClick={() => props.setPage("Tidsrapportering")}>Tidsrapportering</a> : null}
             {loadMember() != null && loadMember().username == "admin" ? <a onClick={() => props.setPage("Medlemmar")}>Medlemmar</a> : null}
-            {loadMember() == null ? (
-                <a onClick={() => props.setPage("Logga in")}>{"Logga in"}</a>
-            ) : (
-                <a
-                    onClick={() => {
-                        props.setPage("Logga in");
+            {loadMember() == null ? (<a onClick={() => props.setPage("Logga in")}>{"Logga in"}</a>)
+                : (<a onClick={() => {
                         removeMember();
-                    }}
-                >
-                    {"Logga ut"}
-                </a>
-            )}
+                        props.setPage("Logga in");
+                    }}>{"Logga ut"}</a>)}
             {loadMember() != null ? null : <a onClick={() => props.setPage("Bli medlem")}>Bli medlem</a>}
         </nav>
     );
