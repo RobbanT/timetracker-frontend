e9f53985f9d557b19b9ebf911b049e174ea71d79
Roberto Tencic <roberto.tencic@outlook.com>
2024-05-04 11:33:36 +0200

    Fix.

--- src/components/TimeTracking/TimeTracking.tsx
+++ src/components/TimeTracking/TimeTracking.tsx
@@ -1,5 +1,5 @@
 import Calendar from "react-calendar";
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import "./style.css";
 
 type ValuePiece = Date | null;
@@ -23,12 +23,32 @@ function loadUser(): User {
 }
 
 function TimeTracking() {
+    const [task, setTask] = useState<Task>({
+        title: "",
+        date: "",
+        startTime: "",
+        endTime: "",
+    });
     const [value, onChange] = useState<Value>(new Date());
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => setTask((values) => ({ ...values, [event.target.name]: event.target.value }));
+
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+    };
     return (
         <>
             <Calendar onChange={onChange} showNeighboringMonth={false} prev2Label={null} next2Label={null} minDate={new Date()} maxDate={new Date(2025, 0, 0)} minDetail={"month"} value={value} />
             <div className="inner-container">
                 <h3>Ny uppgift</h3>
+                <form onSubmit={handleSubmit}>
+                    <label>Användarnamn</label>
+                    <input placeholder="Ange titel..." value={task.title} name="username" onChange={handleChange} minLength={5} maxLength={64} required></input>
+                    <label>Starttid</label>
+                    <input type="datetime-local" placeholder="Ange starttid..." value={task.title} name="username" onChange={handleChange} minLength={5} maxLength={64} required></input>
+                    <label>Sluttid</label>
+                    <input type="datetime-local" placeholder="Ange sluttid..." value={task.title} name="username" onChange={handleChange} minLength={5} maxLength={64} required></input>
+                    <button type="submit">Lägg till uppgift</button>
+                </form>
             </div>
             <div className="inner-container">
                 <h3>Uppgifter</h3>
