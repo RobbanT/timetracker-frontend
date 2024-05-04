b47d885de1a934770af530e37e8247fdd88f5aad
Roberto Tencic <roberto.tencic@outlook.com>
2024-05-04 10:24:11 +0200

    Fix.

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -24,7 +24,7 @@ function Nav(props: Props) {
             ) : (
                 <a
                     onClick={() => {
-                        props.setPage("Hem");
+                        props.setPage("Logga in");
                         removeMember();
                     }}
                 >
