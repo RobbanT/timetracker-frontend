4c57669f923c48805975ac7af94d5fe278ebfcf8
Roberto Tencic <roberto.tencic@outlook.com>
2024-05-06 10:32:43 +0200

    Fix.

--- src/components/TimeTracking/TimeTracking.tsx
+++ src/components/TimeTracking/TimeTracking.tsx
@@ -1,9 +1,5 @@
-import Calendar from "react-calendar";
 import { ChangeEvent, FormEvent, useState } from "react";
 import "./style.css";
-
-type ValuePiece = Date | null;
-type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 interface Task {
     title: string;
@@ -27,7 +23,6 @@ function TimeTracking() {
         startTime: "",
         endTime: "",
     });
-    const [value, onChange] = useState<Value>(new Date());
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => setTask((values) => ({ ...values, [event.target.name]: event.target.value }));
 
     const handleSubmit = (event: FormEvent) => {
@@ -55,7 +50,6 @@ function TimeTracking() {
     };
     return (
         <>
-            <Calendar onChange={onChange} showNeighboringMonth={false} prev2Label={null} next2Label={null} minDate={new Date()} maxDate={new Date(2025, 0, 0)} minDetail={"month"} value={value} />
             <form onSubmit={handleSubmit}>
                 <h3>Ny uppgift</h3>
                 <label>Titel</label>
@@ -68,6 +62,20 @@ function TimeTracking() {
             </form>
             <div className="inner-container">
                 <h3>Uppgifter</h3>
+                <ul>
+                    {loadUser().tasks.map((task: Task) => (
+                        <li>
+                            <p>
+                                Titel
+                                <br />
+                                {task.title}
+                            </p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="inner-container">
+                <h3>Avslutade Uppgifter</h3>
                 <ul>
                     {loadUser().tasks.map((task: Task) => (
                         <li>
