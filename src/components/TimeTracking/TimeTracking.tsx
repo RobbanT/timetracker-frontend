ef860f53618f5f84242f5c73a21b33578de3432a
Roberto Tencic <roberto.tencic@outlook.com>
2024-05-07 10:07:07 +0200

    Fix.

--- src/components/TimeTracking/TimeTracking.tsx
+++ src/components/TimeTracking/TimeTracking.tsx
@@ -22,7 +22,7 @@ function TimeTracking() {
 
     const handleSubmit = (event: FormEvent) => {
         event.preventDefault();
-        fetch(`https://backend-eft68.ondigitalocean.app/user/${task.title}`, {
+        fetch(`https://backend-eft68.ondigitalocean.app/${loadMember().username}/${task.title}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
